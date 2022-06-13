7030e4bca21c9cf25b8f6e316665af0025dcd529
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-06-13 13:40:39 +0530

    modal

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./styles/tailwind-pre-build.css";
 import Card1 from "./CardFinal";
 import Pdp1 from "./pdp1";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Modal from "./modal1";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route>
           <Route exact path="/catalog" component={Card1} />
           <Route path="/pdp" component={Pdp1} />
+          <Route path="/modal" component={Modal} />
         </Route>
       </Switch>
     </Router>
