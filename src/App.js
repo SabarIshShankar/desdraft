97ccb855a2e7308e8d40f54480c626ea178568e8
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2022-06-13 16:56:09 +0530

    disc

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles.css";
 import "./styles/tailwind-pre-build.css";
 import Card1 from "./CardFinal";
 import Pdp1 from "./pdp1";
+import f04 from "./404";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Modal from "./modal1";
+import Discounts from "./discounts";
 
 export default function App() {
   return (
@@ -13,7 +14,8 @@ export default function App() {
         <Route>
           <Route exact path="/catalog" component={Card1} />
           <Route path="/pdp" component={Pdp1} />
-          <Route path="/modal" component={Modal} />
+          <Route path="/discounts" component={Discounts} />
+          <Route path="/f04" component={f04} />
         </Route>
       </Switch>
     </Router>
